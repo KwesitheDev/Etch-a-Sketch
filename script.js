9bc831d18fba80f66d88ad9b4b775ac20c1c55be
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2024-09-16 17:47:16 +0100

    Grid created, pending hovering effect with js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector(".container");
+    const gridSize = 16;
+    const totalSquares = gridSize * gridSize;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        container.appendChild(square);
+    }
+});
